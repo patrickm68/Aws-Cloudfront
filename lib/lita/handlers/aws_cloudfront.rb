75d6417bf6f7c401c93a2e95c822b7a33689be15
patrickm68 <patrick.mcenaney68@gmail.com>
2019-09-03 03:13:08 +0900

    :+1: Create empty command

--- lib/lita/handlers/aws_cloudfront.rb
+++ lib/lita/handlers/aws_cloudfront.rb
@@ -1,7 +1,17 @@
 module Lita
   module Handlers
     class AwsCloudfront < Handler
+      distributions_help = { 'cloudfront distributions' => 'List cloudfront distributions' }
+      route(/cloudfront distributions$/, help: distributions_help) do |response|
+      end
 
+      invalidations_help = { 'cloudfront invalidations' => 'List cloudfront invalidations' }
+      route(/cloudfront invalidations\s+([a-z0-9]+)\s*$/, help: invalidations_help) do |response|
+      end
+
+      invalidate_help = { 'cloudfront invalidate ${distribution_id} ${path}' => 'Invalidate distribution for path' }
+      route(/cloudfront invalidate\s+([a-z0-9]+)\s+(.+)\s*$/, help: invalidate_help) do |response|
+      end
 
       Lita.register_handler(self)
     end
