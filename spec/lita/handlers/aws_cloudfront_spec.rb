db9018fd697b9ad6b22cdbe64c7670f293210c20
patrickm68 <patrick.mcenaney68@gmail.com>
2019-09-06 10:19:00 +0900

    :green_heart: Create spec for invalidations command

--- spec/lita/handlers/aws_cloudfront_spec.rb
+++ spec/lita/handlers/aws_cloudfront_spec.rb
@@ -10,13 +10,14 @@ describe Lita::Handlers::AwsCloudfront, lita_handler: true do
   end
 
   describe 'Behavior' do
+    let(:reply_message) {}
+
     subject { replies }
 
     shared_examples('a command that replies message') { it { is_expected.to include reply_message } }
 
     describe 'distributions command' do
       let(:cloudfront_response) {}
-      let(:reply_message) {}
 
       before do
         allow_any_instance_of(Aws::CloudFront::Client)
@@ -26,7 +27,7 @@ describe Lita::Handlers::AwsCloudfront, lita_handler: true do
 
       context 'with empty distributions response' do
         let(:cloudfront_response) { [] }
-        let(:reply_message) { "There are no invalidations.\n" }
+        let(:reply_message) { "There are no distributions.\n" }
 
         it_behaves_like 'a command that replies message'
       end
@@ -74,6 +75,59 @@ describe Lita::Handlers::AwsCloudfront, lita_handler: true do
     end
 
     describe 'invalidations command' do
+      let(:cloudfront_response) {}
+
+      before do
+        allow_any_instance_of(Aws::CloudFront::Client)
+          .to receive_message_chain(:list_invalidations, :invalidation_list, :items) { cloudfront_response }
+        send_message 'cloudfront invalidations dist'
+      end
+
+      context 'with empty invalidations response' do
+        let(:cloudfront_response) { [] }
+        let(:reply_message) { "There are no invalidations.\n" }
+
+        it_behaves_like 'a command that replies message'
+      end
+
+      context 'with invalidation response' do
+        let(:cloudfront_response) { [OpenStruct.new(id: 1, create_time: '2019/08/07 20:00', status: 'Completed')] }
+        let(:reply_message) do
+          <<~MESSAGE
+            --------------------------------------------------------
+            ID: 1
+            CreatedAt: 2019/08/07 20:00
+            Status: Completed
+            --------------------------------------------------------
+          MESSAGE
+        end
+
+        it_behaves_like 'a command that replies message'
+      end
+
+      context 'with invalidations response' do
+        let(:cloudfront_response) do
+          [
+            OpenStruct.new(id: 1, create_time: '2019/08/07 20:00', status: 'Completed'),
+            OpenStruct.new(id: 2, create_time: '2019/08/07 20:00', status: 'Completed')
+          ]
+        end
+        let(:reply_message) do
+          <<~MESSAGE
+            --------------------------------------------------------
+            ID: 1
+            CreatedAt: 2019/08/07 20:00
+            Status: Completed
+            --------------------------------------------------------
+            ID: 2
+            CreatedAt: 2019/08/07 20:00
+            Status: Completed
+            --------------------------------------------------------
+          MESSAGE
+        end
+
+        it_behaves_like 'a command that replies message'
+      end
     end
 
     describe 'invalidate command' do
