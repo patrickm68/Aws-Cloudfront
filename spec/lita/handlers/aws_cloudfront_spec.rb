a69fab1f3b3b90b2954b4da80682dc663a4404ec
patrickm68 <patrick.mcenaney68@gmail.com>
2019-09-06 09:54:16 +0900

    :green_heart: Add routing spec

--- spec/lita/handlers/aws_cloudfront_spec.rb
+++ spec/lita/handlers/aws_cloudfront_spec.rb
@@ -1,4 +1,11 @@
 require "spec_helper"
 
 describe Lita::Handlers::AwsCloudfront, lita_handler: true do
+  describe 'Routing' do
+    it { is_expected.to route('cloudfront distributions') }
+    it { is_expected.to route('cloudfront invalidations dist') }
+    it { is_expected.to route('cloudfront invalidate dist') }
+    it { is_expected.to route('cloudfront invalidate dist /*') }
+    it { is_expected.to route('cloudfront invalidate dist /images/*') }
+  end
 end
