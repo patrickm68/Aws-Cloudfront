d36f4c5c45628f23dde09a1db55a26579c6ba471
patrickm68 <patrick.mcenaney68@gmail.com>
2019-09-06 10:08:42 +0900

    :+1: Add spec for distributions command

--- spec/lita/handlers/aws_cloudfront_spec.rb
+++ spec/lita/handlers/aws_cloudfront_spec.rb
@@ -8,4 +8,75 @@ describe Lita::Handlers::AwsCloudfront, lita_handler: true do
     it { is_expected.to route('cloudfront invalidate dist /*') }
     it { is_expected.to route('cloudfront invalidate dist /images/*') }
   end
+
+  describe 'Behavior' do
+    subject { replies }
+
+    shared_examples('a command that replies message') { it { is_expected.to include reply_message } }
+
+    describe 'distributions command' do
+      let(:cloudfront_response) {}
+      let(:reply_message) {}
+
+      before do
+        allow_any_instance_of(Aws::CloudFront::Client)
+          .to receive_message_chain(:list_distributions, :distribution_list, :items) { cloudfront_response }
+        send_message 'cloudfront distributions'
+      end
+
+      context 'with empty distributions response' do
+        let(:cloudfront_response) { [] }
+        let(:reply_message) { "There are no invalidations.\n" }
+
+        it_behaves_like 'a command that replies message'
+      end
+
+      context 'with distribution response' do
+        let(:cloudfront_origins) { OpenStruct.new(items: [OpenStruct.new(domain_name: 'origin_domain')]) }
+        let(:cloudfront_response) { [OpenStruct.new(id: 1, domain_name: 'domain_name', origins: cloudfront_origins)] }
+        let(:reply_message) do
+          <<~MESSAGE
+            --------------------------------------------------------
+            ID: 1
+            DomainName: domain_name
+            Origins: origin_domain
+            --------------------------------------------------------
+          MESSAGE
+        end
+
+        it_behaves_like 'a command that replies message'
+      end
+
+      context 'with distributions response' do
+        let(:cloudfront_origins) { OpenStruct.new(items: [OpenStruct.new(domain_name: 'origin_domain')]) }
+        let(:cloudfront_response) do
+          [
+            OpenStruct.new(id: 1, domain_name: 'domain_name1', origins: cloudfront_origins),
+            OpenStruct.new(id: 2, domain_name: 'domain_name2', origins: cloudfront_origins)
+          ]
+        end
+        let(:reply_message) do
+          <<~MESSAGE
+            --------------------------------------------------------
+            ID: 1
+            DomainName: domain_name1
+            Origins: origin_domain
+            --------------------------------------------------------
+            ID: 2
+            DomainName: domain_name2
+            Origins: origin_domain
+            --------------------------------------------------------
+          MESSAGE
+        end
+
+        it_behaves_like 'a command that replies message'
+      end
+    end
+
+    describe 'invalidations command' do
+    end
+
+    describe 'invalidate command' do
+    end
+  end
 end
